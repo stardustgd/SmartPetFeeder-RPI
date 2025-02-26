a30a45c34a6fb1dee7d33348360db54db600c2eb
stardust <88991140+stardustgd@users.noreply.github.com>
2025-02-25 20:07:22 -0800

    feat: start weighing of food dispensing

--- src/dispense.py
+++ src/dispense.py
@@ -1,5 +1,6 @@
 import sys
 import time
+from drivers.hx711 import HX711
 from step import setup, cleanup, rotate_motor
 
 if len(sys.argv) != 2:
@@ -10,8 +11,17 @@ FEEDING_AMOUNT = int(sys.argv[1])
 
 setup()
 
+# Set up hx711
+hx = HX711(dout=5, pd_sck=6)
+hx.setReferenceUnit(-3410)
+
 for i in range(0, FEEDING_AMOUNT):
     rotate_motor()
+
+    rawBytes = hx.readRawBytes()
+    weightValue = hx.rawBytesToWeight(rawBytes)
+    print(f"[INFO] POLLING_BASED | weight (grams): {weightValue}")
     time.sleep(1)
 
+hx.powerDown()
 cleanup()
