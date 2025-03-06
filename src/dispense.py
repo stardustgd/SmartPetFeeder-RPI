dbe074b5df7d775166417aa4dcdc81a4c6ffd1e3
Ian Van Der Heide <ianvanderheide@dhcp-v078-018.mobile.uci.edu>
2025-03-05 18:00:36 -0800

    while loop for feed script

--- src/dispense.py
+++ src/dispense.py
@@ -15,11 +15,17 @@ setup()
 hx = HX711(dout=5, pd_sck=6)
 hx.setReferenceUnit(-3410)
 
-for i in range(0, FEEDING_AMOUNT):
+# for i in range(0, FEEDING_AMOUNT):
+def grams_to_ounces(grams):
+    return grams * 0.03527396
+
+feedAmt = 0
+while (feedAmt < FEEDING_AMOUNT):
     rotate_motor()
 
     rawBytes = hx.readRawBytes()
     weightValue = hx.rawBytesToWeight(rawBytes)
+    feedAmt += grams_to_ounces(weightValue)
     print(f"[INFO] POLLING_BASED | weight (grams): {weightValue}")
     time.sleep(1)
 
