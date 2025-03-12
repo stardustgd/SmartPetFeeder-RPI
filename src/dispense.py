8de68bc1fef162be1b78229d703ec483e5222655
ivanderh <ivanderh@ics.uci.edu>
2025-03-11 18:33:47 -0700

    dispense.py for cups

--- src/dispense.py
+++ src/dispense.py
@@ -7,7 +7,7 @@ if len(sys.argv) != 2:
     print(f"Usage: python {sys.argv[0]} <amount>")
     sys.exit(1)
 
-FEEDING_AMOUNT = int(sys.argv[1])
+FEEDING_AMOUNT = int(sys.argv[1]) * 140
 
 setup()
 
@@ -15,17 +15,13 @@ setup()
 hx = HX711(dout=5, pd_sck=6)
 hx.setReferenceUnit(-3410)
 
-# for i in range(0, FEEDING_AMOUNT):
-def grams_to_ounces(grams):
-    return grams * 0.03527396
-
 feedAmt = 0
 while (feedAmt < FEEDING_AMOUNT):
     rotate_motor()
 
     rawBytes = hx.readRawBytes()
     weightValue = hx.rawBytesToWeight(rawBytes)
-    feedAmt += grams_to_ounces(weightValue)
+    feedAmt += weightValue
     print(f"[INFO] POLLING_BASED | weight (grams): {weightValue}")
     time.sleep(1)
 
