fbfa1d3d296a9d59c4768011355f899db049c00a
stardustgd <88991140+stardustgd@users.noreply.github.com>
2025-03-14 05:19:38 -0700

    feat: dispense (#12)
    * feat: start weighing of food dispensing
    
    * while loop for feed script
    
    * dispense.py for cups
    
    * fix: update load cell reference unit
    
    * feat: use servo instead of steppre
    
    * fix: rotate servo in steps
    
    these steps correspond to each hole in the feeder mechanism
    
    * feat: set servo angle to 0 on KeyboardInterrupt and StopIteration
    
    * chore: remove step.py
    
    * fix: change servo steps
    
    * fix: switch to sliding door method
    
    * fix: load cell configuration
    
    ---------
    
    Co-authored-by: Ian Van Der Heide <ianvanderheide@dhcp-v078-018.mobile.uci.edu>
    Co-authored-by: ivanderh <ivanderh@ics.uci.edu>

--- src/dispense.py
+++ src/dispense.py
@@ -1,17 +1,78 @@
+import board
 import sys
 import time
-from step import setup, cleanup, rotate_motor
+from adafruit_motor import servo
+from adafruit_pca9685 import PCA9685
+from drivers.hx711 import HX711
 
 if len(sys.argv) != 2:
     print(f"Usage: python {sys.argv[0]} <amount>")
     sys.exit(1)
 
 FEEDING_AMOUNT = int(sys.argv[1])
+SERVO_CHANNEL = 3
+SERVO_MIN_PULSE = 500
+SERVO_MAX_PULSE = 3000
+SERVO_ACTUATION_RANGE = 180
 
-setup()
+# Set up servo
+i2c = board.I2C()
+pca = PCA9685(i2c)
+pca.frequency = 50
+servo = servo.Servo(
+    pca.channels[SERVO_CHANNEL],
+    min_pulse=SERVO_MIN_PULSE,
+    max_pulse=SERVO_MAX_PULSE,
+    actuation_range=SERVO_ACTUATION_RANGE,
+)
 
-for i in range(0, FEEDING_AMOUNT):
-    rotate_motor()
-    time.sleep(1)
+servo.angle = 0
+steps = [0, 42]
 
-cleanup()
+# Set up hx711
+hx = HX711(dout=5, pd_sck=6)
+hx.setReferenceUnit(421.03056)
+hx.setReadingFormat("MSB", "MSB")
+hx.autosetOffset()
+
+
+def read_weight(samples=10, delay=0.05):
+    weights = []
+
+    for _ in range(samples):
+        rawBytes = hx.readRawBytes()
+        weightValue = hx.rawBytesToWeight(rawBytes)
+        weights.append(weightValue)
+        time.sleep(delay)
+
+    # Remove outliers
+    weights.remove(max(weights))
+    weights.remove(min(weights))
+
+    return sum(weights) / len(weights)
+
+
+def dispense_food(target_weight):
+    current_weight = read_weight()
+
+    try:
+        while current_weight < target_weight:
+            for step in steps:
+                servo.angle = step
+                time.sleep(0.35)
+
+            servo.angle = 0
+
+            current_weight = read_weight()
+
+            if current_weight >= target_weight:
+                raise StopIteration
+
+            time.sleep(1)
+    except (StopIteration, KeyboardInterrupt):
+        servo.angle = 0
+
+
+dispense_food(FEEDING_AMOUNT)
+print(f"Final weight: {read_weight()}")
+hx.powerDown()
