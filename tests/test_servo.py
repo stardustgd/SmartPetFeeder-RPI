839422247a4d3f15e484738aed23181f07ed3f63
stardustgd <88991140+stardustgd@users.noreply.github.com>
2025-03-13 19:56:40 -0700

    test: move servo in steps (#10)
    * test: create test for servo
    
    * test: create test for dispense
    
    * test: move servo in steps

--- tests/test_servo.py
+++ tests/test_servo.py
@@ -16,11 +16,27 @@ test_servo = servo.Servo(pca.channels[channel], min_pulse=500, max_pulse=3000)
 test_servo.angle = 0
 time.sleep(1)
 
+
+def move_servo(servo, target_angle, step=1, delay=0.02):
+    if servo.angle is None:
+        servo.angle = 0
+
+    current_angle = int(servo.angle)
+
+    step = step if target_angle > current_angle else -step
+
+    for angle in range(current_angle, target_angle, step):
+        servo.angle = angle
+        time.sleep(delay)
+
+    servo.angle = target_angle
+
+
 try:
     while True:
-        angle = input("Enter angle:")
+        angle = int(input("Enter angle:"))
 
-        test_servo.angle = int(angle)
+        move_servo(servo, angle, delay=0.1)
 except KeyboardInterrupt:
     print("Exiting")
-    test_servo.angle = 0
+    move_servo(servo, 0)
